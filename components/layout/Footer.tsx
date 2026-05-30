9f1d924c09e92c88b6e1dfe7b113e865100eb278
benwasden <benjaminpaulwasden@gmail.com>
2026-05-29 19:50:31 -0700

    Footer colors and animation

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 //footer page
 export default function Footer() {
   return (
-    <footer className="w-full border-t bg-amber-100">
+    <footer className="w-full border-t bg-[var(--primary)]">
       <div className="mx-auto flex max-w-7xl flex-col items-center justify-between gap-6 px-6 py-6 md:flex-row">
         <div className="text-center text-gray-800 md:text-left"><span>Team Members:</span>
           <ul className="text-sm space-y-1">
@@ -13,9 +13,9 @@ export default function Footer() {
           </ul>
         </div>
         <div className="flex flex-row gap-4">
-          <a href="https://www.instagram.com/"><Image src="/socialicons/instagram.png" height={40} width={40} alt="Icon of Instagram. Click to visit instagram's website"/></a>
-          <a href="https://www.twitter.com"><Image src="/socialicons/twitter.png" height={40} width={40} alt="Icon of Twitter. Click to visit twitter's website"/></a>
-          <a href="https://www.tiktok.com"><Image src="/socialicons/tik-tok.png" height={40} width={40} alt="Icon of TikTok. Click to visit TikTok's website"/></a>
+          <a href="https://www.instagram.com/"><Image src="/socialicons/instagram.png" height={40} width={40} alt="Icon of Instagram. Click to visit instagram's website" className="transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110"/></a>
+          <a href="https://www.twitter.com"><Image src="/socialicons/twitter.png" height={40} width={40} alt="Icon of Twitter. Click to visit twitter's website" className="transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110"/></a>
+          <a href="https://www.tiktok.com"><Image src="/socialicons/tik-tok.png" height={40} width={40} alt="Icon of TikTok. Click to visit TikTok's website" className="transition duration-200 ease-in-out hover:-translate-y-1 hover:scale-110"/></a>
         </div>
         <p className="text-sm text-gray-800 text-center">
           © 2026 Independent Sheets. All rights reserved.
